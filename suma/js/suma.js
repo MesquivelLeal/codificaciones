9c355cac249a767e75872c9109a2b83960cd27db
milciades <milciade201702@gmail.com>
2024-06-27 16:33:29 -0500

    Trabajo

---
+++ suma/js/suma.js
@@ -0,0 +1,15 @@
+
+//Suma
+
+
+function suma() {
+    let numeroUno;
+    let numeroDos;
+    let resultado;
+
+    numeroUno= 5;
+    numeroDos= 6;
+
+    resultado= numeroUno + numeroDos;
+    return resultado;
+}
